900fd5e97fbb2557890a79627e4a6d8b477a9a4a
Bev Feitosa <bevely1407@gmail.com>
2023-06-17 17:41:22 -0300

    <feat>:adds header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { Home } from "./pages/Home"
+import { HeaderPartial } from "./partials/HeaderPartial"
 
 export const App = () => {
   return (
     <>
+    <HeaderPartial />
     <Home />
     </>
   )
