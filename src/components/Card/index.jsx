92c3b471c13684bf53826d06fb07a09fe928e09d
Bev Feitosa <bevely1407@gmail.com>
2023-07-05 10:05:45 -0300

    <feat>:adds onclick to card

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -2,13 +2,13 @@ import Card from 'react-bootstrap/Card';
 import Badge from 'react-bootstrap/Badge';
 import Button from 'react-bootstrap/Button';
 
-export const CardComponent = ({ image, title, total }) => {
+export const CardComponent = ({ image, title, total, onClick }) => {
   return (
     <Card className="bg-dark text-white">
       <Card.Img src={image} alt="Card image" />
 
       <Card.ImgOverlay>
-        <Button variant="primary">
+        <Button variant="primary" onClick={onClick}>
         Profile <Badge bg="secondary">{total}</Badge>
         </Button>
       </Card.ImgOverlay>
